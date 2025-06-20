f57cbf3df4e5a39fa4a2ab282bdc96d41de9cc6c
William Turner <williamguyturner@proton.me>
2025-06-20 12:31:52 +1200

    Initialise window using new SDL3.0 loop paradigm

--- main.c
+++ main.c
@@ -1,38 +1,44 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+
 #include <stdio.h>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <string.h>
+#include "vec.h"
 
-SDL_Window *window;
-SDL_Renderer *renderer;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
 
-int main() {
-    SDL_Init(SDL_INIT_VIDEO);
-    bool done = false;
+SDL_AppResult SDL_AppInit(void **appState, int argc, char *argv[]) {
+    SDL_SetAppMetadata("Raycast Demo", "0.1", "com.example.willturner");
 
-    window = SDL_CreateWindow(
-        "raycaster",
-        640,
-        480,
-        SDL_WINDOW_OPENGL
-    );
-
-    if (window == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
-        return 1;
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialise SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    while (!done) {
-        SDL_Event event;
-
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
-                done = true;
-            }
-        }
-
-        // Do game logic, present a frame, etc.
+    if(!SDL_CreateWindowAndRenderer("raycaster", 800, 600, 0, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
+    return SDL_APP_CONTINUE;
 }
+
+SDL_AppResult SDL_AppEvent(void *appState, SDL_Event *event) {
+    if (event -> type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appState) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    SDL_RenderPoint(renderer, 50, 50);
+    SDL_RenderPresent(renderer);
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appState, SDL_AppResult result) {}
